26e6acba3e7f103ab60b9e0ed1a3feacd4fc19c3
mjago <martyn.jago@btinternet.com>
2018-01-02 10:10:02 +0000

    Refactor data structures

--- src/gviz.cr
+++ src/gviz.cr
@@ -3,18 +3,38 @@ require "graphviz"
 
 module Gviz
   class Visualizer
-    @data : Data
-
     property data : Data
 
-    struct Data
-      property input : NamedTuple(name: String, size: Int32, values: Array(Float64))
-      property hidden : NamedTuple(count: Int32, sizes: Array(Int32), values: Array(Array(Float64)))
-      property output : NamedTuple(name: String, size: Int32, values: Array(Float64))
+    struct InOut
+      property name : String
+      property size : Int32
+      property values : Array(Float64)
 
-      def initialize(@input = {name: "", size: 0, values: Array(Float64).new},
-                     @output = {name: "", size: 0, values: Array(Float64).new},
-                     @hidden = {count: 1, sizes: Array(Int32).new, values: Array(Array(Float64)).new})
+      def initialize(@name = "",
+                     @size = 0,
+                     @values = Array(Float64).new)
+      end
+    end
+
+    struct Hidden
+      property count : Int32
+      property sizes : Array(Int32)
+      property values : Array(Array(Float64))
+
+      def initialize(@count = 0,
+                     @sizes = Array(Int32).new,
+                     @values = Array(Array(Float64)).new)
+      end
+    end
+
+    struct Data
+      property input : InOut
+      property hidden : Hidden
+      property output : InOut
+
+      def initialize(@input = InOut.new,
+                     @output = InOut.new,
+                     @hidden = Hidden.new)
       end
     end
 
@@ -24,17 +44,41 @@ module Gviz
       @nodes = Array(GraphViz::Node).new
     end
 
+    def input=(val : NamedTuple(name: String, size: Int32))
+      self.input = {name: val[:name], size: val[:size], values: Array(Float64).new}
+    end
+
+    def input=(val)
+      @data.input = InOut.new(val[:name], val[:size], val[:values])
+    end
+
+    def hidden=(val)
+      @data.hidden = Hidden.new(val[:count], val[:sizes], val[:values])
+    end
+
+    def output=(val : NamedTuple(name: String, size: Int32))
+      self.output = {name: val[:name], size: val[:size], values: Array(Float64).new}
+    end
+
+    def output=(val)
+      @data.output = InOut.new(val[:name], val[:size], val[:values])
+    end
+
+    def get_value(values, idx)
+      return "#{idx}" if values.empty?
+      sprintf("%0.2f", values[idx])
+    end
+
     def build_input
-      @graph.add_subgraph(@data.input[:name], GraphViz.new("cluster_0", type: :subgraph)) do |inp|
-        inp[:label] = "#{@data.input[:name]} (#{@data.input[:size].to_s})"
+      @graph.add_subgraph(@data.input.name, GraphViz.new("cluster_0", type: :subgraph)) do |inp|
+        inp[:label] = "#{@data.input.name} (#{@data.input.size})"
         inp[:labelloc] = "top"
         inp[:labeljust] = "center"
         inp[:style] = "filled"
         inp[:fillcolor] = "green"
-        input_size = @data.input[:size]
+        input_size = @data.input.size
         input_size.times do |x|
-          val = @data.input[:values].empty? ? "" : @data.input[:values][x]
-          val_s = sprintf("%0.2f", val)
+          val_s = get_value(@data.input.values, x)
           @nodes << inp.add_node(x.to_s, label: val_s, style: "filled", color: "green", fillcolor: "lightblue")
         end
       end
@@ -45,16 +89,16 @@ module Gviz
         hide[:label] = ""
         hide[:style] = "filled"
         hide[:fillcolor] = "lightgrey"
-        offset = @data.input[:size]
-        hidden_count = @data.hidden[:count]
-        hidden_sizes = @data.hidden[:sizes]
+        offset = @data.input.size
+        hidden_count = @data.hidden.count
+        hidden_sizes = @data.hidden.sizes
         hidden_count.times do |x|
           size = hidden_sizes[x]
           size.times do |y|
-            val = @data.hidden[:values][x].empty? ? "" : @data.hidden[:values][x][y]
+            val = @data.hidden.values[x].empty? ? "" : @data.hidden.values[x][y]
             val_s = sprintf("%0.2f", val)
             @nodes << hide.add_node((offset + y).to_s, label: val_s, style: "filled",
-              color: "blue", fillcolor: "cyan")
+                                    color: "blue", fillcolor: "cyan")
           end
           offset += size
         end
@@ -62,18 +106,17 @@ module Gviz
     end
 
     def build_output
-      @graph.add_subgraph(@data.output[:name], GraphViz.new("cluster_2", type: :subgraph)) do |outp|
-        outp[:label] = "#{@data.output[:name]} (#{@data.output[:size].to_s})"
+      @graph.add_subgraph(@data.output.name, GraphViz.new("cluster_2", type: :subgraph)) do |outp|
+        outp[:label] = "#{@data.output.name} (#{@data.output.size})"
         outp[:labelloc] = "bottom"
         outp[:labeljust] = "center"
         outp[:style] = "filled"
         outp[:fillcolor] = "green"
-        output_size = @data.output[:size]
+        output_size = @data.output.size
         output_size.times do |x|
-          val = @data.output[:values].empty? ? "" : @data.output[:values][x]
-          val_s = sprintf("%0.2f", val)
-          hidden_size = @data.hidden[:sizes].sum
-          offset = @data.input[:size] + hidden_size
+          val_s = get_value(@data.output.values, x)
+          hidden_size = @data.hidden.sizes.sum
+          offset = @data.input.size + hidden_size
           @nodes << outp.add_node((offset + x).to_s, label: val_s, style: "filled",
             color: "blue", fillcolor: "orange")
         end
@@ -81,21 +124,21 @@ module Gviz
     end
 
     def build_input_edges
-      input_size = @data.input[:size]
-      first_hidden_size = @data.hidden[:sizes].first
+      input_size = @data.input.size
+      first_hidden_size = @data.hidden.sizes.first
       input_size.times do |x|
         first_hidden_size.times do |y|
           offset = 0
-          @graph.add_edge(@nodes[offset + x], @nodes[offset + @data.input[:size] + y],
+          @graph.add_edge(@nodes[offset + x], @nodes[offset + @data.input.size + y],
             color: "lightgrey", arrowsize: 1, arrowhead: "empty", style: "dotted")
         end
       end
     end
 
     def build_hidden_edges
-      offset = @data.input[:size]
-      hidden_count = @data.hidden[:count]
-      hidden_sizes = @data.hidden[:sizes]
+      offset = @data.input.size
+      hidden_count = @data.hidden.count
+      hidden_sizes = @data.hidden.sizes
       hidden_count.times do |x|
         next if x == 0
         size1 = hidden_sizes[x - 1]
@@ -111,12 +154,12 @@ module Gviz
     end
 
     def build_output_edges
-      output_size = @data.output[:size]
-      last_hidden_size = @data.hidden[:sizes].last
-      hidden_size = @data.hidden[:sizes].sum
+      output_size = @data.output.size
+      last_hidden_size = @data.hidden.sizes.last
+      hidden_size = @data.hidden.sizes.sum
       last_hidden_size.times do |x|
         output_size.times do |y|
-          offset = @data.input[:size] + hidden_size
+          offset = @data.input.size + hidden_size
           @graph.add_edge(@nodes[offset - last_hidden_size + x], @nodes[offset + y],
             color: "lightgrey", arrowsize: 1, arrowhead: "empty", style: "dotted")
         end
@@ -148,15 +191,17 @@ module Gviz
 end
 
 gv = Gviz::Visualizer.new
-gv.data.input = {name: "Input", size: 8, values: [0.1, 0.2, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3]}
-gv.data.hidden = {count:  4,
-                  sizes:  [12, 14, 10, 8],
-                  values: [[0.1, 0.2, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3],
-                           [0.1, 0.2, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3],
-                           [0.1, 0.2, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3],
-                           [0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3]]}
-gv.data.output = {name:   "Output",
-                  size:   5,
-                  values: [0.1, 0.2, 0.3, 0.2, 0.3]}
+gv.input = {name: "Input",
+            size: 8,
+            values: [0.1, 0.2, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3]}
+gv.hidden = {count:  4,
+             sizes:  [12, 14, 10, 8],
+             values: [[0.1, 0.2, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3],
+                      [0.1, 0.2, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3],
+                      [0.1, 0.2, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3],
+                      [0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3, 0.3]]}
+gv.output = {name:   "Output",
+             size:   5,
+             values: [0.1, 0.2, 0.3, 0.2, 0.3]}
 gv.build
 gv.generate
